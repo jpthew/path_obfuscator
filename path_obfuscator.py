c24f9e3916c72fdd31ad98cd4cbd48d7c5fc24ea
jp <jp@pop-os.localdomain>
2023-06-13 15:00:22 -0500

    Broken still

--- path_obfuscator.py
+++ path_obfuscator.py
@@ -7,9 +7,9 @@ import random
 has_drive = 0
 
 # check running operating system
-if platform.system() == "Windows":
+if platform.system() == 'Windows':
     local_os = 0
-elif platform.system() == "Linux":
+elif platform.system() == 'Linux':
     local_os = 1
 
 # obfuscate string function
@@ -24,7 +24,7 @@ def obfuscate_string(input_string):
             obfuscated_string += char
             previous_char = char
         elif random.randint(0, 1) == 0:
-            if previous_char == "*" and char == "*":
+            if previous_char == '*' and char == '*':
                 continue
             obfuscated_string += '*'
             previous_char = '*'
@@ -34,13 +34,25 @@ def obfuscate_string(input_string):
         previous_char = char
     return obfuscated_string
 
-# check if remote or on local machine
-check = input("Are you checking local path or remote path? (l/r): ").lower()
-if check == "r":
-    path = input("Enter a path: ")  
+# print the obfuscated path
+def print_obfuscated_path():
+    if has_drive == 0:
+        print('Obfuscated path: ' + '/' + '/'.join(path_parts))
+    elif has_drive == 1:
+        print('Obfuscated path: ' + '\\'.join(path_parts))
+
+#re-add drive letter if needed (C: is assumed as default)
+def drivecheck():
+    if has_drive == 1:
+        path_parts.insert(0, 'C:')
+
+check = input('Are you checking local path or remote path? (l/r): ').lower()
+# remote path check
+if check == 'r':
+    path = input('Enter a path: ')  
     # split path variable into parts delimited by a \ or /
-    path_parts = path.split("\\") if "\\" in path else path.split("/")
-    if ":" in path_parts[0]:
+    path_parts = path.split('\\') if '\\' in path else path.split('/')
+    if ':' in path_parts[0]:
         has_drive = 1
         path_parts.pop(0)
     else:
@@ -48,86 +60,66 @@ if check == "r":
     # obfuscate strings in path_parts list with ?, *, or actual letter
     for i in range(len(path_parts)):
         path_parts[i] = obfuscate_string(path_parts[i])
-
-elif check == "l":
-    path = input("Enter a path: ")
+    drivecheck()
+    print_obfuscated_path()
+# local path check
+elif check == 'l':
+    path = input('Enter a path: ')
     # split path variable into parts delimited by a \ or /
-    path_parts = path.split("\\") if "\\" in path else path.split("/")
+    path_parts = path.split('\\') if '\\' in path else path.split('/')
     # search local disk for path and recursivly print all files and folders
     # find unique strings of each UNC path and print them
-    current_path = ""
-    final_filename = ""
+    current_path = ''
+    final_filename = []
     for i in range(len(path_parts)):
-        # removing / from path_parts
-        if "/" in path_parts[i]:
-            path_parts[i] = path_parts[i].replace("/", "")
+
         # recreating next path to listdir
-        current_path = current_path + "/" + path_parts[i]
-        print("Current path: " + current_path)
+        current_path = current_path + '/' + path_parts[i]
+        print('Current path: ' + current_path)
         if os.path.exists(current_path):
             files_found = os.listdir(current_path)
-            print("Files found: " + str(files_found))
+            print('Files found: ' + str(files_found))
             # Time for a stupid nested ifs and fors :D Hopefully this works
             for j in range(len(files_found)):
+
                 if files_found[j] not in path_parts[i]:
                     new_file_list = []
                     matchy = ''
-                    print("Found a unique string: " + files_found[j])
+                    print('Found a unique string: ' + files_found[j])
+
                     for k in range(len(path_parts[i])):
                         # randomize everything, all the time
+
                         for l in range(len(files_found[j]), len(path_parts[i])):
-                            files_found[j] = files_found[j] + "*"
+                            files_found[j] = files_found[j] + '*'
+
                         if path_parts[i][k] != files_found[j][k]:
                             if random.randint(0,1) == 1:
                                 new_file_list.append(path_parts[i][k])
                                 matchy = files_found[j][k]
                             elif random.randint(0, 1) == 0:
                                 if random.randint(0, 1) == 1:
-                                    if matchy == "*":
+                                    if matchy == '*':
                                         continue
                                     else:
-                                        new_file_list.append("*")
-                                        matchy = "*"
+                                        new_file_list.append('*')
+                                        matchy = '*'
                             elif random.randint(0, 1) == 0:
-                                new_file_list.append("?")
-                                matchy = "?"
+                                new_file_list.append('?')
+                                matchy = '?'
                         else:
                             if random.randint(0, 1) == 1:
-                                if matchy == "*":
+                                if matchy == '*':
                                     continue
                                 else:
-                                    new_file_list.append("*")
-                                    matchy = "*"
+                                    new_file_list.append('*')
+                                    matchy = '*'
                             elif random.randint(0, 1) == 0:
-                                new_file_list.append("?")
-                                matchy = "?"
-
-                    if len(new_file_list[j]) < len(path_parts[i]):
-                        if new_file_list[-1] != "*" or path_parts[i][-1]:
-                            new_file_list.append("*") 
-                        if new_file_list[0] != "*" or path_parts[i][0]:
-                            new_file_list.insert(0, "*")   
-                    str_filename = "".join(new_file_list)
-                    final_filename = str_filename + "/"               
-                    print("new filename = " + final_filename)
+                                new_file_list.append('?')
+                                matchy = '?'
+                    new_file_list.append('/')
         else: 
-            print("Path does not exist")
+            print('Path does not exist')
             sys.exit()
-
-
-
-
-#### print all parts of the path based on the length of the path_parts list REMOVE ME AFTER TESTING ####
-for i in range(len(path_parts)):
-    print(path_parts[i])
-
-#re-add drive letter if needed (C: is assumed as default)
-if has_drive == 1:
-    path_parts.insert(0, "C:")
-
-# print the obfuscated path
-if has_drive == 0:
-    print("Obfuscated path: " + "/".join(path_parts))
-elif has_drive == 1:
-    print("Obfuscated path: " + "\\".join(path_parts))
-    
+    drivecheck()
+    print_obfuscated_path()
