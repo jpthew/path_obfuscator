be1b5c4b2a8edec58079196a1d93853d6ea30d02
JP <jp@thewnet.work>
2023-06-14 14:31:57 -0500

    First functional release of both modes.

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # path_obfuscator
-Obfuscates linux and windows paths
+Obfuscates linux and windows UNCs
+
+I created this script to create user-obfuscated, but machine readable paths. It works on all OS that take wildcard characters in the UNC.
+
+Functions as a simple, not-nesissarily smart obfuscator for UNCs. It is / and \ agnostic, but stay consistent with your path. It does not (intentionally) escape characters.
+There are two functions, remote and local UNCs.
+
+Remote takes any path without validation and obfuscates it the best it can. It can take a few runs of this to have a properly formatted obfuscation, so try at your own risk. This mode has a possible chance to not work due to wildcards overextending the file name size. WIP
+
+Remote example:
+``` bash
+$ python3 path_obfuscator.py 
+$ Are you checking local path or remote path? (l/r): r
+$ Enter a path: C:\windows\system32
+$ Obfuscated path: C:\*i***w*\?ys?****
+```
+
+Local takes the path and checks it against your local filesystem. If the path is correct and has no issues checking, then it recursively compares your pathname to similar pathnames (ls'ing every folder) to try and obfuscate common letters, and reveal unique characters. It creates the same obfuscated file every time, if the file structure remains the same on the host OS. This mode should always have a "correct" obfuscated path, but always double-check.
+Invalid folder structure will result in this mode failing. If you need to try a theoretical path, use the "Remote" type.
+
+Local Example:
+ ``` bash
+$ python3 path_obfuscator.py 
+$ Are you checking local path or remote path? (l/r): l
+$ Enter a path: /home/jp/Desktop
+$ Obfuscated path: /h?me/jp/*?k*p
+```
+
+Issues to fix:
+L: Path with single folder showing complete folder
+L: Windows paths tending to overobfuscate in extremely large folders (e.g. system32), machine unreadable.
+R: Foldername length overwriting
--- path_obfuscator.py
+++ path_obfuscator.py
@@ -37,87 +37,97 @@ def obfuscate_string(input_string):
 # print the obfuscated path
 def print_obfuscated_path():
     if has_drive == 0:
-        print('Obfuscated path: ' + '/' + '/'.join(path_parts))
+        print('Obfuscated path: ' + '/' + '/'.join(l_obfuscated))
     elif has_drive == 1:
-        print('Obfuscated path: ' + '\\'.join(path_parts))
+        print('Obfuscated path: ' + '\\'.join(l_obfuscated))
 
 #re-add drive letter if needed (C: is assumed as default)
 def drivecheck():
     if has_drive == 1:
-        path_parts.insert(0, 'C:')
+        l_obfuscated.insert(0, 'C:')
+
+# compare list and obfuscate on the 'against' parameter
+def compare_obfuscate(list, against):
+    new_list = []
+    new_path = []
+    # word formatting: extending word, create wordlist, removing word if complete match (should always happen)
+    for word in list:
+        if word == against:
+            continue
+        if len(word) < len(against):
+                word_delta = len(against) - len(word)
+                word = word + ('?' * word_delta)
+        for i in range(len(against)):
+            if word[i] == against[i]:
+                #print(word)
+                new_list.append(word)
+                break
+            
+
+    # masking on the 'against' parameter from the wordlist
+    for i in range(len(against)):
+        var = 0
+        for word in new_list:
+            if word[i] == against[i]:
+                var = 1
+        if var == 1:
+            new_path.append('?')
+        elif var == 0:
+            new_path.append(against[i])
+    
+    new_path_str =  "".join(new_path)
+    new_path_str = new_path_str.replace('??', '*')
+    return new_path_str
 
 check = input('Are you checking local path or remote path? (l/r): ').lower()
 # remote path check
 if check == 'r':
     path = input('Enter a path: ')  
     # split path variable into parts delimited by a \ or /
-    path_parts = path.split('\\') if '\\' in path else path.split('/')
-    if ':' in path_parts[0]:
+    path_part = path.split('\\') if '\\' in path else path.split('/')
+    if ':' in path_part[0]:
         has_drive = 1
-        path_parts.pop(0)
+        path_part.pop(0)
     else:
         has_drive = 0
     # obfuscate strings in path_parts list with ?, *, or actual letter
-    for i in range(len(path_parts)):
-        path_parts[i] = obfuscate_string(path_parts[i])
+    for p_iterator in range(len(path_part)):
+        path_part[p_iterator] = obfuscate_string(path_part[p_iterator])
+        l_obfuscated = path_part
     drivecheck()
     print_obfuscated_path()
+
+
 # local path check
 elif check == 'l':
     path = input('Enter a path: ')
-    # split path variable into parts delimited by a \ or /
-    path_parts = path.split('\\') if '\\' in path else path.split('/')
-    # search local disk for path and recursivly print all files and folders
-    # find unique strings of each UNC path and print them
+    path_part = path.split('\\') if '\\' in path else path.split('/')
+    if ':' in path_part[0]:
+        has_drive = 1
+        path_part.pop(0)
+    else:
+        has_drive = 0
+    for i in range(len(path_part)):
+        if path_part[i] == '':
+            path_part.pop(i)
+            break
     current_path = ''
     final_filename = []
-    for i in range(len(path_parts)):
+    for p_iterator in range(len(path_part)):
 
         # recreating next path to listdir
-        current_path = current_path + '/' + path_parts[i]
-        print('Current path: ' + current_path)
-        if os.path.exists(current_path):
-            files_found = os.listdir(current_path)
-            print('Files found: ' + str(files_found))
+        current_path = '/'
+        if os.path.exists(path):
+            l_obfuscated = []
+            #print('Files found: ' + str(files_found))
             # Time for a stupid nested ifs and fors :D Hopefully this works
-            for j in range(len(files_found)):
-
-                if files_found[j] not in path_parts[i]:
-                    new_file_list = []
-                    matchy = ''
-                    print('Found a unique string: ' + files_found[j])
-
-                    for k in range(len(path_parts[i])):
-                        # randomize everything, all the time
-
-                        for l in range(len(files_found[j]), len(path_parts[i])):
-                            files_found[j] = files_found[j] + '*'
-
-                        if path_parts[i][k] != files_found[j][k]:
-                            if random.randint(0,1) == 1:
-                                new_file_list.append(path_parts[i][k])
-                                matchy = files_found[j][k]
-                            elif random.randint(0, 1) == 0:
-                                if random.randint(0, 1) == 1:
-                                    if matchy == '*':
-                                        continue
-                                    else:
-                                        new_file_list.append('*')
-                                        matchy = '*'
-                            elif random.randint(0, 1) == 0:
-                                new_file_list.append('?')
-                                matchy = '?'
-                        else:
-                            if random.randint(0, 1) == 1:
-                                if matchy == '*':
-                                    continue
-                                else:
-                                    new_file_list.append('*')
-                                    matchy = '*'
-                            elif random.randint(0, 1) == 0:
-                                new_file_list.append('?')
-                                matchy = '?'
-                    new_file_list.append('/')
+            for i in range(len(path_part)):
+                files_found = os.listdir(current_path)
+                check = path_part[i]
+                obfuscated_string = compare_obfuscate(files_found, check)
+                l_obfuscated.append(obfuscated_string)
+                current_path += path_part[i] + '/'
+            break
         else: 
             print('Path does not exist')
             sys.exit()
